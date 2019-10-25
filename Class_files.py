8c7d4d9ac5f7831bc4fb33f42152acba0592d4e0
JYH <whdudghks22@gmail.com>
2019-10-25 12:06:40 +0900

    맵,캐릭터,몬스터 화면 띄우기
    맵,몬스터,화면 띄움

--- Class_files.py
+++ Class_files.py
@@ -1,31 +1,31 @@
 import random
 import json
 import os
-#import pause_state
+# import pause_state
 
 from pico2d import *
 
 import game_framework
 import Title_state
 
-
-
 name = "Class_files"
 
 tengo = None
 grass = None
 font = None
+slime = None
+map = None
+x = 0
+y = 0
+count = 1
 
-x=0
-y=0
-count=1
 
 class Grass:
-    #def __init__(self):
-      #  self.image = load_image('grass.png')
+    # def __init__(self):
+    #  self.image = load_image('grass.png')
 
-    #def draw(self):
-        #self.image.draw(400, 30)
+    # def draw(self):
+    # self.image.draw(400, 30)
     pass
 
 
@@ -35,26 +35,48 @@ class Tengo:
         self.frame = 0
         self.image = load_image('tengo_sleep.png')
 
-
     def update(self):
         self.frame = (self.frame + 1) % 12
-
 
     def draw(self):
         self.image.clip_draw(self.frame * 200, 0, 200, 200, self.x, self.y)
 
 
+
+class Slime:
+    def __init__(self):
+        self.x, self.y = 300, 90
+        self.frame = 0
+        self.image = load_image('slime_sleep.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 7
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 200, 0, 200, 200, self.x, self.y)
+
+
+class Map:
+    def __init__(self):
+        self.image = load_image("Map1.png")
+
+    def draw(self):
+        self.image.draw(683, 384)
+
+
 def enter():
-    global tengo,grass
+    global tengo, grass, slime,map
     tengo = Tengo()
     grass = Grass()
-
-
+    slime = Slime()
+    map=Map()
 
 def exit():
-    global tengo, grass
+    global tengo, grass, slime,map
     del (tengo)
     del (grass)
+    del (slime)
+    del (map)
 
 
 def pause():
@@ -70,26 +92,25 @@ def handle_events():
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
-         game_framework.quit()
+            game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-         game_framework.change_state(Title_state)
+            game_framework.change_state(Title_state)
         elif event.type == SDL_KEYDOWN and event.key == SDLK_p:
-            x,y=event.x,event.y
-            #game_framework.push_state(pause_state2)
-
+            x, y = event.x, event.y
+            # game_framework.push_state(pause_state2)
 
 
 def update():
     tengo.update()
+    slime.update()
 
 
 def draw():
     clear_canvas()
-    #grass.draw()
+    # grass.draw()
+    map.draw()
     tengo.draw()
+    slime.draw()
+    delay(0.1)
+
     update_canvas()
-
-
-
-
-
--- Game_Start.py
+++ Game_Start.py
@@ -4,6 +4,6 @@ import game_framework
 import game_framework
 import pico2d
 import Logo_start_state
-pico2d.open_canvas()
+pico2d.open_canvas(1366,768)
 game_framework.run(Logo_start_state)
 pico2d.close_canvas()
