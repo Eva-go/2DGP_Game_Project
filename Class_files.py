7b342625693a5c885d2f5955a0024bb97643a142
JYH <whdudghks22@gmail.com>
2019-10-25 12:42:28 +0900

    맵,캐릭터,몬스터 위치수정
    맵,캐릭터,몬스터 위치수정 및 클래스 저장

--- Class_files.py
+++ Class_files.py
@@ -15,6 +15,7 @@ grass = None
 font = None
 slime = None
 map = None
+
 x = 0
 y = 0
 count = 1
@@ -31,7 +32,7 @@ class Grass:
 
 class Tengo:
     def __init__(self):
-        self.x, self.y = 50, 90
+        self.x, self.y = 266, 300
         self.frame = 0
         self.image = load_image('tengo_sleep.png')
 
@@ -41,11 +42,9 @@ class Tengo:
     def draw(self):
         self.image.clip_draw(self.frame * 200, 0, 200, 200, self.x, self.y)
 
-
-
 class Slime:
     def __init__(self):
-        self.x, self.y = 300, 90
+        self.x, self.y = 1166, 300
         self.frame = 0
         self.image = load_image('slime_sleep.png')
 
@@ -65,14 +64,15 @@ class Map:
 
 
 def enter():
-    global tengo, grass, slime,map
+    global tengo, grass, slime, map
     tengo = Tengo()
     grass = Grass()
     slime = Slime()
-    map=Map()
+    map = Map()
+
 
 def exit():
-    global tengo, grass, slime,map
+    global tengo, grass, slime, map
     del (tengo)
     del (grass)
     del (slime)
@@ -97,6 +97,8 @@ def handle_events():
             game_framework.change_state(Title_state)
         elif event.type == SDL_KEYDOWN and event.key == SDLK_p:
             x, y = event.x, event.y
+
+
             # game_framework.push_state(pause_state2)
 
 
