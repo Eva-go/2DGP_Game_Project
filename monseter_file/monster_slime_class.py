cce4cb6a88815232df33a8109d7edae48f5dc6e7
Eva <whdudghks22@gmail.com>
2019-11-24 06:34:19 +0900

    시간 수정
    시간 수정

--- monseter_file/monster_slime_class.py
+++ monseter_file/monster_slime_class.py
@@ -1,28 +1,38 @@
 from pico2d import *
 import random
+import game_framework
 
-tmp = 10
-
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 10
 
 class Slime:
-    image = None
 
     def __init__(self, num):
         self.x, self.y = 866 + 100 * num, 300
         self.frame = random.randint(0, 6)
         self.hp=20
-
-        if Slime.image == None:
-            self.image = load_image('monseter_file\\slime_sleep.png')
-
-        self.layer = 2
-
+        self.sleep = Animation('monseter_file\\slime_sleep.png',7,200,200)
+        self.image = self.sleep
     def update(self):
-        global tmp
-        self.frame += 1
-        if self.frame / tmp >= 7:
+
+        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 7
+        if self.frame >= self.image.max_frame:
             self.frame = 0
 
     def draw(self):
-        global tmp
-        self.image.clip_draw((self.frame // tmp) * 200, 0, 200, 200, self.x, self.y)
+
+        self.image.clip_draw(int(self.frame), 0, 200, 200, self.x, self.y)
+
+
+
+class Animation:
+    def __init__(self, path, max_frame, image_w, image_h):
+        self.image = load_image(path)
+        self.max_frame = max_frame
+
+        self.image_h = image_h
+        self.image_w = image_w
+
+    def clip_draw(self, image_x, image_y, w, h, x, y):
+        self.image.clip_draw(image_x * self.image_w, image_y, self.image_w, self.image_h, x, y)
--- player_file/player_tengo_class.py
+++ player_file/player_tengo_class.py
@@ -1,53 +1,54 @@
 from pico2d import *
 import random
 import Class_files
+import game_framework
 
-tmp = 10
+PIXEL_PER_METER = (10.0 / 0.3)
+RUN_SPEED_KMPH = 20.0
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 10
 
 
 class Tengo:
 
     def __init__(self):
-        global tmp
         self.x, self.y = 266, 350
         self.frame = 0
         self.hp = 50
-        self.image_count=False
+        self.image_count = 0.0
         self.sleep = Animation('player_file\\tengo_sleep.png', 12, 200, 200)
         self.attack = Animation('player_file\\tengo_attack.png', 11, 400, 300)
-        #self.skill = Animation('player_file\\.png', 21, 351, 129)
-        #self.skill = Animation('player_file\\skills.png,',21,173,127)
+        # self.skill = Animation('player_file\\.png', 21, 351, 129)
+        # self.skill = Animation('player_file\\skills.png,',21,173,127)
 
         self.image = self.sleep
 
         self.layer = 1
 
+    #def do(self):
+       # self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % Animation.max_frame
+
     def draw(self):
-        self.image.clip_draw((self.frame // tmp), 0, 200, 200, self.x, self.y)
+        self.image.clip_draw(int(self.frame), 0, 200, 200, self.x, self.y)
 
         pass
-    def update(self):
-        global tmp
-        self.frame += 1
-        if self.frame / tmp >= self.image.max_frame:
-            self.frame = 0
 
+    def update(self):
+        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % self.image.max_frame
+        if self.frame>= self.image.max_frame:
+            self.frame = 0
         if Class_files.tengo_attack:
             self.image = self.attack
-            if self.frame == 11:
-                Class_files.tengo_attack =False
-
-
-
-        #elif Class_files.tengo_shield == True:
-            # self.image = self.skill
-            pass
-        else:
-            self.image = self.sleep
-
+            if self.frame+(FRAMES_PER_ACTION*ACTION_PER_TIME)%self.image.max_frame==self.image.max_frame:
+                self.image = self.sleep
 
 class Animation:
-    def __init__(self, path, max_frame,image_w, image_h):
+    def __init__(self, path, max_frame, image_w, image_h):
         self.image = load_image(path)
         self.max_frame = max_frame
 
@@ -56,4 +57,3 @@ class Animation:
 
     def clip_draw(self, image_x, image_y, w, h, x, y):
         self.image.clip_draw(image_x * self.image_w, image_y, self.image_w, self.image_h, x, y)
-
